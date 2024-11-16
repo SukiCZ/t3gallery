107b82112b0c82436d633c54b4dc8bc4298501e4
Jakub Boukal <www.bagr@gmail.com>
2024-11-16 10:08:57 +0100

    Use auth on homepage

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,10 +1,17 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export function TopNav() {
     return (
         <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-            <div>Gallery</div>
-
+            <div>
+                <Link className="p-4" href="/">
+                    Home
+                </Link>
+                {/*<Link className="p-4" href="/gallery">*/}
+                {/*    Gallery*/}
+                {/*</Link>*/}
+            </div>
             <div>
                 <SignedIn>
                     <UserButton />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,60 @@
 import Image from "next/image";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
+import { currentUser } from '@clerk/nextjs/server'
+
+
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
     const posts = await db.query.posts.findMany({
         with: {
             images: true,
-        }
+        },
     });
 
     return (
+        <div className="flex flex-wrap gap-4">
+            {posts.map((post) => (
+                <div key={post.id} className="w-48">
+                    <div>{post.name}</div>
+                    {post.images.map(
+                        (image: { id: number; url: string }) => (
+                            <Image
+                                key={image.id}
+                                src={image.url}
+                                alt="image"
+                                className="w-full"
+                                width={500}
+                                height={500}
+                            />
+                        ),
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+export default async function HomePage() {
+    const user = await currentUser();
+
+
+    return (
         <main className="">
-            <div className="flex flex-wrap gap-4">
-                {posts.map((post) => (
-                    <div key={post.id} className="w-48">
-                        <div>{post.name}</div>
-                        {(post.images || []).map((image: {id: number, url: string}) => (
-                            <Image key={image.id} src={image.url} alt="image" className="w-full" width={500} height={500} />
-                        ))}
-                    </div>
-                ))}
-            </div>
+            <SignedOut>
+                <div className="h-full w-full p-16 text-2xl text-center">
+                    Please sign in
+                </div>
+            </SignedOut>
+            <SignedIn>
+                <div className="h-full w-full p-16 text-2xl text-center">
+                    Welcome {user?.fullName}
+                </div>
+                <Images />
+            </SignedIn>
         </main>
     );
 }
